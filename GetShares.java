cd02c66624ce1fc23c47a45398fde5fd4cd7abee
spapapan <lucid.gr@gmail.com>
2015-11-29 15:25:23 +0200

    Update GetShares.java

--- GetShares.java
+++ GetShares.java
@@ -15,10 +15,8 @@ public class GetShares {
 	public int getShares(String url){
 		
 		 int FB = 0;
-		 int TW = 0;
 		
 		 String getFB = facebook(url);
-		 String getTW = twitter(url);
 		 
 		 String[] splited = getFB.split("\\b+");
 		 if (Arrays.asList(splited).contains("shares")){
@@ -26,11 +24,9 @@ public class GetShares {
 			 FB = Integer.parseInt(getFB);
 		 }
 
-		 getTW = getTW.substring(getTW.indexOf("count\":") + 7, getTW.indexOf(","));
-		 TW = Integer.parseInt(getTW);
 		 
 		 
-		 return (FB + TW);
+		 return (FB);
 		 
 		
 	}
@@ -75,42 +71,5 @@ public class GetShares {
 	}
 	
     
-    // ---- Get Twitter Share Count ---
-	private String twitter(String surl){
-		
-		String url ="https://cdn.api.twitter.com/1/urls/count.json?url="+surl; 
-		try {
-		URL obj = new URL(url);
-		HttpURLConnection con;
-		
-			con = (HttpURLConnection) obj.openConnection();
-			
-			
-			con.setRequestMethod("GET");
-			//add request header
-			con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-
-			// Send request
-			//System.out.println("\nSending 'GET' request to URL : " + url);
-			BufferedReader in = new BufferedReader(
-			new InputStreamReader(con.getInputStream()));
-			String outputLine;
-
-			// reading output from Request
-			StringBuffer response = new StringBuffer(); 
-			while ((outputLine = in.readLine()) != null) {
-			   response.append(outputLine);
-			   
-			}
-			in.close();
-			return response.toString();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
 
 }
